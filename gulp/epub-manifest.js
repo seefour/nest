2b9c830c367c31f7f71442ad8abd214991b0d537
sh0ji <sh0ji@users.noreply.github.com>
2016-10-08 21:03:40 -0400

    optimized to be more gulp-y

--- gulp/epub-manifest.js
+++ gulp/epub-manifest.js
@@ -1,10 +1,10 @@
 'use strict';
 
-import glob from 'glob'
-import mime from 'mime'
 import path from 'path'
-import pug from 'pug'
 import fs from 'fs'
+import globby from 'globby'
+import mime from 'mime'
+import pug from 'pug'
 
 export default function(gulp, plugins, browserSync, options) {
     let config = options.config
@@ -14,65 +14,67 @@ export default function(gulp, plugins, browserSync, options) {
     let template = path.join(dirs.source, dirs.content, dirs.layouts, entries.manifest)
     let dest = path.join(options.target, dirs.main)
 
+    // helper function to ensure IDs are unique
+    const ids = []
+    function makeIdUnique(id) {
+        let root = id
+        let n = 0
+        do {
+            n++
+            id = `${root}-${n}`
+        } while (ids.indexOf(id) >= 0)
+        return id
+    }
+
+    // build the manifest array
+    function manifestMap(files) {
+        let manifest = []
+        for (let file of files) {
+            let mimetype = mime.lookup(path.extname(file))
+
+            // id string: ext-filename
+            let id = `${path.extname(file).replace('.', '')}-${path.basename(file, path.extname(file))}`
+            if (ids.indexOf(id) >= 0) makeIdUnique(id)
+            ids.push(id)
+            if (mimetype !== 'application/octet-stream') {
+                let item = {
+                    id: id,
+                    path: path.relative(dest, file),
+                    properties: '', // todo: generate properties
+                    type: mimetype
+                }
+                manifest.push(item)
+            }
+        }
+        return manifest
+    }
+
     // Manifest -> OEBPS/package.opf
     return (done) => {
-        let manifestMap = (files) => {
-            // create the manifest array
-            let manifest = []
-            let ids = []
-            for (var i = 0; i < files.length; i++) {
-                let mimetype = mime.lookup(path.extname(files[i]))
-                let id = `${path.extname(files[i]).replace('.', '')}-${path.basename(files[i], path.extname(files[i]))}`
-                if (ids.indexOf(id) >= 0) {
-                    let root = id
-                    let n = 0
-                    do {
-                        n++
-                        id = `${root}-${n}`
-                    } while (ids.indexOf(id) >= 0)
+
+        // create timestamp with format YYYY-MM-DDTHH:MM:00Z
+        let timestamp = new Date()
+        timestamp.setSeconds(Math.round(timestamp.getSeconds() / 60) * 60)
+        config.metadata.modified = timestamp.toISOString().replace(/\.\d+Z/g, 'Z')
+
+        // build the manifest array
+        let files = globby.sync(path.join(dest, '**/*'))
+        let manifestData = manifestMap(files)
+
+        // build the manifest opf file from the pug template
+        gulp.src(template)
+            .pipe(plugins.changed(dest))
+            .pipe(plugins.pug({
+                pug: pug,
+                pretty: true,
+                locals: {
+                    config: config,
+                    debug: true,
+                    files: manifestData
                 }
-                ids.push(id)
-                if (mimetype !== 'application/octet-stream') {
-                    let item = {
-                        id: id,
-                        path: path.relative(dest, files[i]),
-                        properties: '',
-                        type: mimetype
-                    }
-                    manifest.push(item)
-                }
-            }
-
-            // create timestamp with format YYYY-MM-DDTHH:MM:00Z
-            let timestamp = new Date()
-            timestamp.setSeconds(Math.round(timestamp.getSeconds() / 60) * 60)
-            config.metadata.modified = timestamp.toISOString().replace(/\.\d+Z/g, 'Z')
-
-            // build the manifest opf file from the pug template
-            return gulp.src(template)
-                .pipe(plugins.changed(dest))
-                .pipe(plugins.plumber())
-                .pipe(plugins.pug({
-                    pug: pug,
-                    pretty: true,
-                    locals: {
-                        config: config,
-                        debug: true,
-                        files: manifest
-                    }
-                }))
-                .pipe(plugins.rename(function(path) {
-                    path.extname = '.opf'
-                }))
-                .pipe(gulp.dest(dest))
-        }
-
-        glob(path.join(dest, '**/*'), function(err, data) {
-            if (err) {
-                console.error(err)
-            }
-            return manifestMap(data)
-        })
+            }))
+            .pipe(plugins.rename((path) => path.extname = '.opf'))
+            .pipe(gulp.dest(dest))
         done()
     }
 }
