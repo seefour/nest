64b39255ce6c53ffb75eafe3a32ab9712342dbe8
sh0ji <sh0ji@users.noreply.github.com>
2016-10-11 00:25:48 -0400

    optimizations and sync handling for gulp.series()

--- gulp/browserify.js
+++ gulp/browserify.js
@@ -1,46 +1,79 @@
 'use strict'
 
 import path from 'path'
+import es from 'event-stream'
+import glob from 'glob'
 import browserify from 'browserify'
 import watchify from 'watchify'
 import envify from 'envify'
 import babelify from 'babelify'
+import vbuffer from 'vinyl-buffer'
+import vsource from 'vinyl-source-stream'
 
 export default function(gulp, plugins, browserSync, options) {
-    let args = options.args
-    let config = options.config
-    let dirs = config.directories
-    let entries = path.join(dirs.source, dirs.scripts, config.entries.js)
-    let dest = path.join(options.target, dirs.main, dirs.scripts.replace('_', ''))
+    const args = options.args
+    const config = options.config
+    const dirs = config.directories
+    const entries = path.join('./', dirs.source, dirs.scripts, config.entries.js)
+    const dest = path.join(options.target, dirs.main)
 
     return (done) => {
-        let startTime = new Date().getTime()
-        gulp.src(entries, {read: false})
-            .pipe(plugins.tap((file) => {
-                plugins.util.log(
-                    'Bundled',
-                    plugins.util.colors.cyan(path.relative(dirs.source, file.path))
-                )
-                file.contents = browserify(file.path, {
+        glob(entries, (err, files) => {
+            if (err) done(err)
+
+            return files.map((entry) => {
+                // default options
+                const defaultOpts = {
+                    entries: [entry],
                     debug: (!args.production) ? true : false,
                     transform: [
                         babelify,
                         envify
                     ]
-                }).bundle()
-            }))
-            .pipe(plugins.buffer())
-            .pipe(plugins.if(!args.production,
-                plugins.sourcemaps.init({loadMaps: true}))
-            )
-            .pipe(plugins.if(args.production, plugins.uglify()))
-            .on('error', plugins.notify.onError(config.defaultNotification))
-            .pipe(plugins.rename((filepath) => {
-                filepath.dirname = filepath.dirname.replace(dirs.source, '').replace('_', '')
-            }))
-            .pipe(plugins.if(!args.production, plugins.sourcemaps.write('./')))
-            .pipe(gulp.dest(dest))
-            .on('end', (file) => browserSync.reload('*.js'))
-        done()
+                }
+
+                // browserify instance
+                let b = browserify(defaultOpts)
+
+                // make it a watchify instance for non-production
+                if (!args.production) {
+                    let watchOpts = Object.assign({}, watchify.args, defaultOpts)
+                    b = watchify(browserify(watchOpts))
+                }
+
+                const bundleTask = () => {
+                    let startTime = new Date().getTime()
+                    b.bundle()
+                        .on('error', plugins.notify.onError(config.defaultNotification))
+                        .pipe(vsource(entry))
+                        .pipe(vbuffer())
+                        // sourcemaps for non-production build
+                        .pipe(plugins.if(!args.production, plugins.sourcemaps.init({loadMaps: true})))
+                        // uglify for production build
+                        .pipe(plugins.if(args.production, plugins.uglify()))
+                        .on('error', plugins.notify.onError(config.defaultNotification))
+                        // src/_scripts/**/*.js -> scripts/**/*.js
+                        .pipe(plugins.rename((filepath) => filepath.dirname = filepath.dirname.replace(dirs.source, '').replace('_', '')))
+                        .pipe(plugins.if(!args.production, plugins.sourcemaps.write('./')))
+                        .pipe(gulp.dest(dest))
+                        .on('end', () => {
+                            let time = (new Date().getTime() - startTime) / 1000
+                            console.log(`${plugins.util.colors.cyan(path.relative(path.join(dirs.source, dirs.scripts), entry))} was browserified in ${plugins.util.colors.magenta(time+' s')}`)
+                            // signal completion synchronously for production builds
+                            if (files.indexOf(entry) === files.length - 1) done()
+                            return browserSync.reload('*.js')
+                        })
+                }
+
+                if (!args.production) {
+                    b.on('update', bundleTask)
+                    b.on('log', plugins.util.log)
+                }
+
+                return bundleTask()
+            })
+        })
+        // signal asynchronous completion for non-production
+        if (!args.production) done()
     }
 }
