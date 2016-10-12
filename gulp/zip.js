9eb6b4ca318bdb291e5e86072a840976ec353d9f
Evan Yamanishi <sh0ji@users.noreply.github.com>
2016-10-12 17:53:46 -0400

    epub file has to begin with mime type

--- gulp/zip.js
+++ gulp/zip.js
@@ -3,16 +3,23 @@
 import path from 'path'
 
 export default function(gulp, plugins, browserSync, options) {
+    const dirs = options.config.directories
+
     // only run on production files
-    let target = path.join(
-        options.config.directories.destination,
+    const target = path.join(
+        dirs.destination,
         `${options.config.metadata.title}_${options.date}`
     )
-    let buildDir = path.join(target, '**/*')
+
+    // epub must start with the mimetype file
+    const buildFiles = [
+        path.join(target, 'mimetype'),
+        path.join(target, '**/*')
+    ]
 
     // Zip -> turns build_dir/ into build_dir.epub
     return (done) => {
-        gulp.src(buildDir)
+        gulp.src(buildFiles)
             .pipe(plugins.zip(`${target}.epub`))
             .pipe(gulp.dest('./'))
             .on('end', () => done())
