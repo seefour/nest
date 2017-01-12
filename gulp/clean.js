cc9bf4093538c9275eda9fa4ff4111a81cb9baf4
Evan Yamanishi <sh0ji@users.noreply.github.com>
2017-01-12 15:03:45 -0500

    added force & mac args

--- gulp/clean.js
+++ gulp/clean.js
@@ -6,13 +6,24 @@ import del from 'del'
 export default function(gulp, p, browserSync, options) {
     const dirs = options.config.directories
     const title = options.config.metadata.productcode
+    const date = options.build.date.short
+
+    // `gulp clean --force` to remove everything from ./build
+    const delDir = (options.args.force) ? [
+        path.resolve(dirs.destination),
+        path.resolve(dirs.temporary)
+
+    // `gulp clean --mac` to remove .DS_Store files
+    ] : (options.args.mac) ? [
+        path.resolve('**/.DS_Store')
+
+    // `gulp clean` (no argument) to remove today's builds
+    ] : [
+        path.join(dirs.destination, `${title}_${date}`),
+        path.join(dirs.destination, `${title}_${date}.*`),
+        path.resolve(dirs.temporary)
+    ]
 
     // Clean
-    return (done) => {
-            del([
-            path.join(dirs.destination, `${title}_${options.date}`),
-            path.join(dirs.destination, `${title}_${options.date}.*`),
-            path.resolve(dirs.temporary)
-        ]).then(() => done())
-    }
+    return (done) => del(delDir).then(() => done())
 }
