a2423244315f6f6c8c84623cbd6f2b7952c60f36
Evan Yamanishi <sh0ji@users.noreply.github.com>
2017-01-12 15:05:40 -0500

    use default imagemin plugins for now

--- gulp/imagemin.js
+++ gulp/imagemin.js
@@ -1,28 +1,25 @@
 'use strict'
 
 import path from 'path'
-import pngquant from 'imagemin-pngquant'
 
-export default function(gulp, plugins, browserSync, options) {
-    let args = options.args
-    let config = options.config
-    let dirs = config.directories
-    let entries = config.entries
-    let dest = path.join(options.target, dirs.main, dirs.images.replace(/^_/, ''))
+export default function(gulp, p, browserSync, options) {
+    const config = options.config
+    const dirs = config.directories
+    const entry = path.join(dirs.source, dirs.images)
+    const dest = path.join(options.target, dirs.main, dirs.images.replace(/^_/, ''))
+
+    const formats = '{[^_]**/*.{gif,ico,jpeg,jpg,png,svg},[^_]*.{gif,ico,jpeg,jpg,png,svg}}'
 
     // Imagemin
     return (done) => {
-        gulp.src(path.join(dirs.source, dirs.images, '**/*.{jpg,jpeg,gif,svg,png}'))
-            .pipe(plugins.changed(dest))
-            .pipe(plugins.if(args.production, plugins.imagemin({
-                progressive: true,
-                svgoPlugins: [{
-                    removeViewBox: false
-                }],
-                use: [pngquant({
-                    speed: 10
-                })]
-            })))
+        gulp.src(path.join(entry, formats), {
+                base: entry
+            })
+            .pipe(p.changed(dest))
+            .pipe(p.imageResize({
+                width: config.globals['image-widths'].max
+            }))
+            .pipe(p.imagemin())
             .pipe(gulp.dest(dest))
             .on('end', () => done())
     }
